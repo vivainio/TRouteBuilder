44067acf6e5569691d00f0c103a2b2ebc71b7cf6
Ville M. Vainio <ville.vainio@basware.com>
2017-11-24 20:24:36 +0200

    working

--- RouteBuilder/Program.cs
+++ RouteBuilder/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TrivialRouteBuilder;
+using System.Web.Http;
 
-namespace RouteBuilder
+
+namespace Test.TrivialRouteBuilder
 {
+
+    [RoutePrefix("hello")]
+    class FakeController1 : ApiController
+    {
+        [Route("world/{id}")]
+        public void GetStuff(string id)
+        {
+
+        }
+    }
+
+    class FakeController2 : ApiController
+    {
+        [Route("hello/world")]
+        public void GetStuff()
+        {
+
+        }
+    }
+
+    class FakeController3 : ApiController
+    {
+        [Route("{a}/{b}/{c}")]
+        public string ManyVars(string a, string b, int c)
+        {
+            return a + b + c.ToString();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var url1 = RouteBuilder.ResolveRoute<FakeController1>(c => c.GetStuff("foo"))
+                .Fill("id", "1212");
+
+            var url2 = RouteBuilder.ResolveRoute<FakeController2>(c => c.GetStuff());
+            var fail = RouteBuilder.Fill("foo/{id}", "id", "world");
+
+            var linker = new RelativeUrlBuilder("/myapproot");
+            var rel = linker.To<FakeController3>(c => c.ManyVars("", "", 0)).Fill("a", "12", "b", "13", "c", "14");
         }
     }
 }
